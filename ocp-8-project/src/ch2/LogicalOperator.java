b0a781e357e1ed3002a824f223fd6e27a8090c9f
Mohammad Najar <mmotieian@yahoo.com>
2018-07-04 16:10:51 -0500

    logical operators

---
+++ ocp-8-project/src/ch2/LogicalOperator.java
@@ -0,0 +1,25 @@
+package ch2;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		int b = 20;
+		System.out.println(++a > 10 || b++ < 20);
+		System.out.println(b);
+		
+		Byte bite = new Byte("-128");
+		System.out.println(bite);
+		
+		
+		String str = "22";
+		Integer.parseInt(str);
+		
+		System.out.println(Boolean.parseBoolean("TrUe"));
+		
+		
+//		bite.
+	}
+
+}
