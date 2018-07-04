f653adc152c2eb2e52de33cbdbe7d1de048e60ef
Mohammad Najar <mmotieian@yahoo.com>
2018-07-04 00:25:42 -0500

    removing metadata info

--- ocp-8-project/src/ch2/DataTypes.java
+++ ocp-8-project/src/ch2/DataTypes.java
@@ -1,10 +1,38 @@
 package ch2;
 
 public class DataTypes {
-
+	static Double dd;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		double d = 33;
+		int $i = (int)d;
+		
+		String s1= "jj";
+		String s2 = "ii";
+		String s3 = s1+s2;
+		
+		char c = 'a';
+		System.out.println(++c);
+		
+		System.out.println(s3);
+		
+		
+		
+		int a = 10;
+		a = a++ + a + a-- - a-- + ++a;
+		System.out.println(a);
+		
+		a = 10;
+		a = a++ * a;
+		System.out.println(a);
+		
+		
+		a = 10;
+		a = ++a + a + --a - --a + a++;
+		System.out.println(a);
 	}
 
 }
